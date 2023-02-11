9262203827e02d0011c0656a739ebbc5f6e9a7dc
riccardoNovaglia <riccardo.novaglia@gmail.com>
2023-02-11 16:53:06 +0000

    Phone util fn

--- src/dsl/utils.ts
+++ src/dsl/utils.ts
@@ -2,8 +2,7 @@ import { left_shift } from "./modifiers";
 import { SingleToInput, ToInput } from "./types";
 
 export function email(email: string): ToInput {
-  const chars: string[] = email.split("");
-  return chars.map((char) => {
+  return stringToChars(email, (char) => {
     switch (char) {
       case "@":
         return left_shift("2");
@@ -13,4 +12,20 @@ export function email(email: string): ToInput {
         return char as SingleToInput;
     }
   });
+}
+
+export function phone(phoneNumber: string): ToInput {
+  return stringToChars(phoneNumber, (char) => {
+    switch (char) {
+      case "+":
+        return left_shift("equal_sign");
+      default:
+        return char as SingleToInput;
+    }
+  });
+}
+
+function stringToChars(string: string, transform: (char: string) => SingleToInput): ToInput {
+  const chars: string[] = string.split("");
+  return chars.map(transform);
 }
