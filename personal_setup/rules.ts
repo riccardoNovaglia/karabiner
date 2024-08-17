fe6d6bf9ee730e778933a4a1714c7b6809fce23d
riccardoNovaglia <riccardo.novaglia+github@gmail.com>
2024-08-17 13:49:39 +0100

    Rules tweaks
    Clearer definitions of cool, noice, hyper.
    Moved app rules to Raycast.
    Removed keebs specific rules.

--- personal_setup/rules.ts
+++ personal_setup/rules.ts
@@ -1,8 +1,5 @@
 import {
-  app,
-  appBundle,
   AppRule,
-  DeviceRule,
   from,
   hyper,
   left_command,
@@ -10,155 +7,180 @@ import {
   left_opt,
   left_shift,
   modKey,
-  multiApp,
   noice,
   pasteEmoji,
-  pycharmProject,
   right_command,
-  right_opt,
   Rule,
   stc,
 } from "karabiner-ts";
 import { KarabinerRules } from "karabiner-ts/build/k/types";
 import { privateRules } from "./private";
 
-const k8 = { productId: 591, vendorId: 1452 };
-const q11 = { productId: 480, vendorId: 13364 };
-
-const easyVolume = [
-  from("f10").to("mute"),
-  from("f11").to("volume_decrement"),
-  from("f12").to("volume_increment"),
-];
-const inAppRules = [
-  AppRule("Zoom", "us.zoom.xos", [
-    from("f1").to(modKey("a", ["left_command", "left_shift"])), // toggle audio
-    from("f2").to(modKey("v", ["left_command", "left_shift"])), // toggle video
-    ...easyVolume,
-  ]),
-  AppRule("Meet", "com.apple.Safari", [
-    from("f1").to(left_command("d")), // toggle audio
-    from("f2").to(left_command("e")), // toggle video
-    from("grave_accent_and_tilde").to(modKey("m", ["left_control", "left_command"])), // toggle self view
-    from("f5").to(modKey("c", ["left_control", "left_command"])), // toggle chat panel
-    // Q11
-    from("f21").to(left_command("d")), // toggle audio
-    from("f22").to(left_command("e")), // toggle video
-    from("f23").to(modKey("m", ["left_control", "left_command"])), // toggle self view
-    from("f24").to(modKey("c", ["left_control", "left_command"])), // toggle chat panel
-    ...easyVolume,
-  ]),
-  AppRule("Meet", "com.google.Chrome.app.kjgfgldnnfoeklkmfkjfagphfepbbdan", [
-    from("f1").to(left_command("d")), // toggle audio
-    from("f2").to(left_command("e")), // toggle video
-    from("grave_accent_and_tilde").to(modKey("m", ["left_control", "left_command"])), // toggle self view
-    from("f5").to(modKey("c", ["left_control", "left_command"])), // toggle chat panel
-    // Q11
-    from("f21").to(left_command("d")), // toggle audio
-    from("f22").to(left_command("e")), // toggle video
-    from("f23").to(modKey("m", ["left_control", "left_command"])), // toggle self view
-    from("f24").to(modKey("c", ["left_control", "left_command"])), // toggle chat panel
-    ...easyVolume,
-  ]),
-  AppRule("Zoom - Q11", "us.zoom.xos", [
-    from("f21").to(modKey("a", ["left_command", "left_shift"])), // toggle audio
-    from("f22").to(modKey("v", ["left_command", "left_shift"])), // toggle video
-    ...easyVolume,
-  ]),
-  AppRule("Spotify", "com.spotify.client", [
-    from("f7").to("rewind"),
-    from("f9").to("fastforward"),
-    from(left_command("f")).to(left_command("l")),
-    from(left_command("left_arrow")).to(modKey("left_arrow", ["left_command", "left_option"])),
-    from(left_command("right_arrow")).to(modKey("right_arrow", ["left_command", "left_option"])),
-    ...easyVolume,
-  ]),
-  AppRule("Slack", "com.tinyspeck.slackmacgap", [
-    from(left_opt("w")).to(modKey("h", ["left_command", "left_shift"])),
-    from(left_command("l")).to(modKey("s", ["left_command", "left_shift"])),
-    from(modKey("left_arrow", ["left_command", "left_option"])).to(left_command("open_bracket")),
-    from(modKey("right_arrow", ["left_command", "left_option"])).to(left_command("close_bracket")),
-    from(left_command("1")).to(left_ctrl("1")),
-    from(left_command("2")).to(left_ctrl("2")),
-    from(left_command("3")).to(left_ctrl("3")),
-    from(left_command("4")).to(left_ctrl("4")),
-  ]),
-  AppRule("Arc", "company.thebrowser.Browser", [
-    from(left_opt("tab")).to(left_ctrl("tab")),
-    from(modKey("tab", ["left_shift", "left_option"])).to(
-      modKey("tab", ["left_shift", "left_control"])
-    ),
-    from(left_command("k")).to(left_command("t")),
-  ]),
-];
-const openApps = Rule("Apps", [
-  from(right_command("b")).to(app("Arc")),
-  from(right_command("c")).to(appBundle("com.google.Chrome")),
-  from(right_command("g")).to(app("Gmail")),
-  from(right_command("i")).to(app("iTerm")),
-  from(right_command("k")).to(pycharmProject("~/projects/kraken-core")),
-  from(right_command("l")).to(appBundle("com.google.Chrome.app.kjbdgfilnfhdoflbpgamdcdgpehopbep")),
-  from(right_command("n")).to(app("Notion")),
-  from(right_command("s")).to(app("Slack")),
-  from(right_command("v")).to(app("Vivaldi")),
-  from(right_command("y")).to(app("Spotify")),
-  from(right_command("x")).to(appBundle("com.jetbrains.pycharm")),
-]);
-const noiceHyperNavigate = Rule("Noice/Hyper navigate", [
-  from(noice("j")).to("left_arrow"),
-  from(noice("k")).to("down_arrow"),
-  from(noice("l")).to("right_arrow"),
-  from(noice("i")).to("up_arrow"),
-  // allow selecting with shift
-  from(hyper("j")).to(left_shift("left_arrow")),
-  from(hyper("k")).to(left_shift("down_arrow")),
-  from(hyper("l")).to(left_shift("right_arrow")),
-  from(hyper("i")).to(left_shift("up_arrow")),
-]);
-
-const emojis = Rule("Emojis", [
-  from(right_command("e")).to(hyper("e")),
-  from(noice("p")).to(pasteEmoji("🐼")),
-]);
-const softwarey = Rule("Softwarey stuff", [
-  from(left_opt("open_bracket")).to(stc("{%")),
-  from(left_opt("close_bracket")).to(stc("%}")),
-  from(left_opt("hyphen")).to(stc("->")),
-  from(left_opt("equal_sign")).to(stc("=>")),
-  from(left_opt("a")).to([...stc("() => {}"), "left_arrow"]),
-  from(right_opt("r")).to(stc("return")),
-  from(modKey("i", ["left_control", "left_command"])).to(stc("__init__.py")),
-]);
-const suggestion = Rule("Suggestion", [
-  from(noice("s")).to(
-    stc(
-      `\`\`\`suggestion
-
-\`\`\``
-    )
-  ),
-]);
-export const rules: KarabinerRules[] = [
-  Rule("Caps lock to escape", from("caps_lock").to("escape")),
-  DeviceRule("Fn to right-control switch", k8, [
-    from("fn").to("right_control"),
-    from("right_control").to("fn"),
-  ]),
-  DeviceRule("Easy delete", k8, from(left_ctrl("delete_or_backspace")).to("delete_forward")),
-  Rule("Easy percent", from(left_opt("p")).to(stc("%"))),
-  Rule("Easy ampersand", from(left_opt("e")).to(stc("&"))),
-  Rule("Pause on f8 by default", [from("f8").to("play_or_pause"), from(left_shift("f8")).to("f8")]),
-  Rule("Right Opt to Noice", [
+function modifiersMappings() {
+  // Cool: Cmd, Opt, Shift
+  const rightShiftToCool = Rule("Right Shift to Cool", [
+    from("right_shift").to(modKey("left_command", ["left_option", "left_shift"])),
+  ]);
+  // Noice: Cmd, Opt, Ctrl
+  const rightOptToNoice = Rule("Right Opt to Noice", [
     from("right_alt").to(modKey("left_control", ["left_option", "left_command"])),
-    // from(noice("period")).to(modKey("period", ["left_shift"])),
-    // from(noice("comma")).to(modKey("comma", ["left_shift"])),
-  ]),
-  emojis,
-  openApps,
-  noiceHyperNavigate,
-  softwarey,
-  suggestion,
-  ...inAppRules,
+  ]);
+  // Hyper: Cmd, Opt, Ctrl, Shift
+  const rightCmdToHyper = Rule("Right Cmd to Hyper", [
+    from("right_command").to(modKey("left_control", ["left_shift", "left_option", "left_command"])),
+  ]);
+  return [rightShiftToCool, rightOptToNoice, rightCmdToHyper];
+}
+
+function simpleRemaps() {
+  const easyVolume = [
+    from("f10").to("mute"),
+    from("f11").to("volume_decrement"),
+    from("f12").to("volume_increment"),
+  ];
+
+  const inAppRules = [
+    AppRule("Fleet", "com.jetbrains.fleet", [from("f12").to(modKey("f12", ["left_option"]))]),
+    AppRule("Zoom", "us.zoom.xos", [
+      from("f1").to(modKey("a", ["left_command", "left_shift"])), // toggle audio
+      from("f2").to(modKey("v", ["left_command", "left_shift"])), // toggle video
+      ...easyVolume,
+    ]),
+    AppRule("Meet", "com.apple.Safari", [
+      from("f1").to(left_command("d")), // toggle audio
+      from("f2").to(left_command("e")), // toggle video
+      from("grave_accent_and_tilde").to(modKey("m", ["left_control", "left_command"])), // toggle self view
+      from("f5").to(modKey("c", ["left_control", "left_command"])), // toggle chat panel
+      // Q11
+      from("f21").to(left_command("d")), // toggle audio
+      from("f22").to(left_command("e")), // toggle video
+      from("f23").to(modKey("m", ["left_control", "left_command"])), // toggle self view
+      from("f24").to(modKey("c", ["left_control", "left_command"])), // toggle chat panel
+      ...easyVolume,
+    ]),
+    AppRule("Meet", "com.google.Chrome.app.kjgfgldnnfoeklkmfkjfagphfepbbdan", [
+      from("f1").to(left_command("d")), // toggle audio
+      from("f2").to(left_command("e")), // toggle video
+      from("grave_accent_and_tilde").to(modKey("m", ["left_control", "left_command"])), // toggle self view
+      from("f5").to(modKey("c", ["left_control", "left_command"])), // toggle chat panel
+      // Q11
+      from("f21").to(left_command("d")), // toggle audio
+      from("f22").to(left_command("e")), // toggle video
+      from("f23").to(modKey("m", ["left_control", "left_command"])), // toggle self view
+      from("f24").to(modKey("c", ["left_control", "left_command"])), // toggle chat panel
+      ...easyVolume,
+    ]),
+    AppRule("Zoom - Q11", "us.zoom.xos", [
+      from("f21").to(modKey("a", ["left_command", "left_shift"])), // toggle audio
+      from("f22").to(modKey("v", ["left_command", "left_shift"])), // toggle video
+      ...easyVolume,
+    ]),
+    AppRule("Spotify", "com.spotify.client", [
+      from("f7").to("rewind"),
+      from("f9").to("fastforward"),
+      from(left_command("f")).to(left_command("l")),
+      from(left_command("left_arrow")).to(modKey("left_arrow", ["left_command", "left_option"])),
+      from(left_command("right_arrow")).to(modKey("right_arrow", ["left_command", "left_option"])),
+      ...easyVolume,
+    ]),
+    AppRule("Slack", "com.tinyspeck.slackmacgap", [
+      from(left_opt("w")).to(modKey("h", ["left_command", "left_shift"])),
+      from(left_command("l")).to(modKey("s", ["left_command", "left_shift"])),
+      from(modKey("left_arrow", ["left_command", "left_option"])).to(left_command("open_bracket")),
+      from(modKey("right_arrow", ["left_command", "left_option"])).to(
+        left_command("close_bracket")
+      ),
+      from(left_command("1")).to(left_ctrl("1")),
+      from(left_command("2")).to(left_ctrl("2")),
+      from(left_command("3")).to(left_ctrl("3")),
+      from(left_command("4")).to(left_ctrl("4")),
+    ]),
+    AppRule("Arc", "company.thebrowser.Browser", [
+      from(left_opt("tab")).to(left_ctrl("tab")),
+      from(modKey("tab", ["left_shift", "left_option"])).to(
+        modKey("tab", ["left_shift", "left_control"])
+      ),
+      from(left_command("k")).to(left_command("t")),
+      from("f12").to(modKey("j", ["left_command", "left_option"])),
+    ]),
+    AppRule("Chrome - 1pass remap", "com.google.Chrome", [
+      from(modKey("backslash", ["left_command", "left_shift"])).to(
+        modKey("x", ["left_command", "left_shift"])
+      ),
+    ]),
+  ];
+
+  const capsToEscape = Rule("Caps lock to escape", [
+    from("caps_lock").to("escape"),
+    from(left_shift("caps_lock")).to("escape"),
+  ]);
+  const easyPct = Rule(
+    "Easy percent",
+    from(modKey("p", ["left_option", "left_shift"])).to(stc("%"))
+  );
+  const easyAmp = Rule(
+    "Easy ampersand",
+    from(modKey("e", ["left_option", "left_shift"])).to(stc("&"))
+  );
+  const noMin = Rule(
+    "No mo minimise",
+    from(left_command("m")).to(modKey("m", ["left_command", "left_shift"]))
+  );
+  const pauseF8 = Rule("Pause on f8 by default", [
+    from("f8").to("play_or_pause"),
+    from(left_shift("f8")).to("f8"),
+  ]);
+  const home = Rule("Home", [from(noice("up_arrow")).to(left_command("home"))]);
+  const end = Rule("End", [from(noice("down_arrow")).to(left_command("end"))]);
+
+  return [...inAppRules, capsToEscape, easyPct, easyAmp, noMin, pauseF8, home, end];
+}
+
+// const hyperApps = Rule("Apps", [ // Setup in Raycast instead
+//   from(hyper("b")).to(app("Arc")),
+//   from(hyper("c")).to(appBundle("com.google.Chrome")),
+//   from(hyper("g")).to(app("Gmail")),
+//   from(hyper("l")).to(appBundle("com.google.Chrome.app.kjbdgfilnfhdoflbpgamdcdgpehopbep")), // caLendar
+//   from(hyper("m")).to(app("com.google.Chrome.app.kjgfgldnnfoeklkmfkjfagphfepbbdan")), // Meet
+//   from(hyper("n")).to(app("Notion")),
+//   from(hyper("s")).to(app("Slack")),
+//   from(hyper("t")).to(app("Terminal")),
+//   from(hyper("v")).to(app("Vivaldi")),
+//   from(hyper("y")).to(app("Spotify")),
+//   from(hyper("x")).to(appBundle("com.jetbrains.pycharm")),
+//   from(hyper("1")).to(appBundle("com.1password.1password")),
+// ]);
+
+function textReplace() {
+  const emojis = Rule("Emojis", [from(noice("p")).to(pasteEmoji("🐼"))]);
+  const suggestion = Rule("Suggestion", [
+    from(noice("s")).to(
+      stc(
+        `\`\`\`suggestion
+  
+  \`\`\``
+      )
+    ),
+  ]);
+  const softwarey = Rule("Softwarey stuff", [
+    from(left_opt("open_bracket")).to(stc("{%")),
+    from(left_opt("close_bracket")).to(stc("%}")),
+    from(left_opt("hyphen")).to(stc("->")),
+    from(left_opt("equal_sign")).to(stc("=>")),
+    from(left_opt("a")).to([...stc("() => {}"), "left_arrow"]),
+    from(left_opt("i")).to(stc("__init__.py")),
+  ]);
+
+  return [emojis, softwarey, suggestion];
+}
+
+export const rules: KarabinerRules[] = [
+  ...modifiersMappings(),
+  ...simpleRemaps(),
+  ...textReplace(),
+
+  // hyperApps,
+
   ...privateRules,
 ];
 
--- src/dsl/modifiers.ts
+++ src/dsl/modifiers.ts
@@ -16,10 +16,16 @@ export const left_command = (k: KeyCode): ModdedKeyCode => modKey(k, ["left_comm
 export const left_opt = (k: KeyCode): ModdedKeyCode => modKey(k, ["left_option"]);
 export const right_opt = (k: KeyCode): ModdedKeyCode => modKey(k, ["right_option"]);
 export const left_shift = (k: KeyCode): ModdedKeyCode => modKey(k, ["left_shift"]);
+export const right_shift = (k: KeyCode): ModdedKeyCode => modKey(k, ["right_shift"]);
 export const right_command = (k: KeyCode): ModdedKeyCode => modKey(k, ["right_command"]);
 
+// Cool: Cmd, Opt, Shift
+export const cool = (key: KeyCode): ModdedKeyCode =>
+  modKey(key, ["left_command", "left_option", "left_shift"]);
+// Noice: Cmd, Opt, Ctrl
 export const noice = (key: KeyCode): ModdedKeyCode =>
   modKey(key, ["left_command", "left_option", "left_control"]);
+// Hyper: Cmd, Opt, Ctrl, Shift
 export const hyper = (key: KeyCode): ModdedKeyCode =>
   modKey(key, ["left_command", "left_option", "left_control", "left_shift"]);
 
@@ -38,6 +44,6 @@ export function vsCodeProject(path: string): Shell {
 }
 export function pycharmProject(path: string): Shell {
   return {
-    shell_command: `~/.idea/cmd/pycharm ${path}`,
+    shell_command: `~/.idea/cmd/charm ${path}`,
   };
 }
